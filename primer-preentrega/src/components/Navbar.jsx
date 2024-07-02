6f0b2d7c26c2841dc4d18065955ac5f267fba0da
Maria Jose Rojo <99.rojomaria@gmail.com>
2024-07-01 21:49:25 -0300

    Estructura base Navbar

---
+++ primer-preentrega/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <div className="navbar-title">Mi Empresa</div>
+        <ul className="navbar-links">
+          <li><a href="#categoria1">Categoría 1</a></li>
+          <li><a href="#categoria2">Categoría 2</a></li>
+          <li><a href="#categoria3">Categoría 3</a></li>
+        </ul>
+      </nav>
+    );
+  }
+
+  export default Navbar
