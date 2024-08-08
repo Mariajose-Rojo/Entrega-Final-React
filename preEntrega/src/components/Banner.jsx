63fc9ea4e387ce4f5e29c9dc1b5e425eaae9ecfc
Maria Jose Rojo <99.rojomaria@gmail.com>
2024-08-08 02:26:08 -0300

    Acomode medida del banner y carrousel (width y center)

--- preEntrega/src/components/Banner.jsx
+++ preEntrega/src/components/Banner.jsx
@@ -1,12 +1,15 @@
 const Banner = () =>{
     return(
-        <div className="container">
-            <div className="row">
-                <div className="row  img-banner py-5">
+        <div className="container-banner">
+            <div className="row mt-0" >
+                <div className="row img-banner py-5">
                 </div>
-                <div className="col text-center py-1">
-                    <h2>FARMACITY</h2>
-                    <p>Encontrá tus productos de dermocosmética, belleza, cuidado personal, farmacia y mucho más</p>
+                
+            </div>
+            <div className="row mt-0">
+                 <div className="col text-center">
+                    <h2 className="text-center mt-3"><b>FARMACITY</b></h2>
+                    <p className="card-text mt-1 fs-4 mb-3" >Encontrá tus productos de dermocosmética, belleza, cuidado personal, farmacia y mucho más</p>
                 </div>
             </div>
         </div>
--- preEntrega/src/components/Carrousel.jsx
+++ preEntrega/src/components/Carrousel.jsx
@@ -1,7 +1,8 @@
 const Carrousel = () => {
     return (
         <>
-            <div id="carouselExampleIndicators" className="carousel slide mb-4" data-bs-ride="carousel" data-bs-interval="2000">
+        <div className="carrousel-container">
+            <div id="carouselExampleIndicators" className="carousel slide mb-1" data-bs-ride="carousel" data-bs-interval="2000">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -32,6 +33,7 @@ const Carrousel = () => {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
+        </div>
         </>
     );
 }
